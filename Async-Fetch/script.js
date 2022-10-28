e622a5034e881a39b827069da8ce36cad2138d31
Kyubs1610 <diego.decort@gmail.com>
2022-10-28 11:47:40 +0200

    morning commit

---
+++ Async-Fetch/script.js
@@ -0,0 +1,24 @@
+const body = document.querySelector('body');
+const button = document.createElement('button');
+body.appendChild(button)
+button.innerText=('Click me please')
+const ul = document.createElement ('ul');
+body.appendChild(ul);
+
+button.addEventListener('click', () => { 
+    fetch('becode.json')
+        .then(response => response.json())
+        // convert the file in JSON
+        .then(data => {
+            console.log(data)
+            for (let i = 0; i < data.length; i++) {
+                let li = document.createElement("li");
+                li.innerHTML = data[i];
+                ul.appendChild(li);
+              }  
+        })
+    .catch(error => {
+      console.log('There was an error!', error)
+    })
+
+})
